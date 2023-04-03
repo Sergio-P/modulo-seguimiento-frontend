2dbfab76bf8790ad6e651d9d9e75996bdcb3395b
Nicolás Lemuñir <nicolas.lemunir2112@gmail.com>
2023-04-03 16:46:09 -0400

    feat(tailwind): add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5E5384",
+        secondary: "#225497",
+        error: "#EB5757",
+        accent: "#FB7A08",
+        body: "#4E4B66",
+        title: "#2C2948",
+      },
+    },
   },
   plugins: [],
 };
