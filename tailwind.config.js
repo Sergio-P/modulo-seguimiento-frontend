2bdec8985503cc88049b3039790337a6df24ea8a
Nicolás Lemuñir <nicolas.lemunir2112@gmail.com>
2023-04-03 17:06:37 -0400

    feat(tailwind): update color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,19 @@ module.exports = {
       colors: {
         primary: "#5E5384",
         secondary: "#225497",
+        background: {
+          light: "#FCFCFC",
+          DEFAULT: "#EFF0F6",
+          dark: "#D9DBE9",
+        },
+        font: {
+          DEFAULT: "#111111",
+          title: "#2C2948",
+          subtitle: "#6E7191",
+        },
         error: "#EB5757",
         accent: "#FB7A08",
         body: "#4E4B66",
-        title: "#2C2948",
       },
     },
   },
