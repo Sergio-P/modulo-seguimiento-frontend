86fbee93af261897e5d0704b4de52e4bb92cfae5
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-04-04 14:28:09 -0400

    adding button svg

--- src/components/cases/CaseForm.tsx
+++ src/components/cases/CaseForm.tsx
@@ -30,16 +30,35 @@ export default function CaseForm(props: CaseFormProps) {
               Seguimientos
             </div>
           </Button>
-          <Button>Historial</Button>
+          <Button className="flex items-center gap-3">
+            <div className="translate-y-[1px]">
+              <Image src="/icons/FileIcon.svg" width={16} height={16} alt="" />
+            </div>
+            <div className="text-sm font-semibold tracking-wide">
+              Historial
+            </div>
+          </Button>
         </div>
       </div>
 
       <div className="px-5 pt-8">
-        <div className="w-600 grid grid-cols-4 gap-4 rounded-xl border-2 border-primary p-4">
-          <h1 className="font-bold">{data.patient.name}</h1>
+        <div className="w-600 flex justify-between rounded-xl border-2 border-primary p-4">
+          <h1 className="font-bold flex-none">{data.patient.name}</h1>
           <Foo label={"Rut"} value={data.patient.rut} />
           <Foo label={"Registro"} value={data.patient.registro} />
           <Foo label={"Ficha"} value={data.patient.ficha} />
+          <div className="flex justify-between">
+            <Button type="filled" className="flex items-center gap-3">
+              <div className="translate-y-[1px]">
+                <Image src="/icons/2cuadrados.svg" width={16} height={16} alt="" />
+              </div>
+            </Button>
+            <Button type="filled" className="flex items-center gap-3">
+              <div className="translate-y-[1px]">
+                <Image src="/icons/2cuadrados.svg" width={16} height={16} alt="" />
+              </div>
+            </Button>
+          </div>
         </div>
       </div>
     </div>
