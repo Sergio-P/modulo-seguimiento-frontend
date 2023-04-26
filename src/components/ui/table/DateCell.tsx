b29f1851a317b059af0dc2c563a38ccecc274b13
ElficTitious <ismael.correa.25@gmail.com>
2023-04-25 21:32:49 -0400

    fix: DateCell expected value of date to be of type Date, but backend returns strings

--- src/components/ui/table/DateCell.tsx
+++ src/components/ui/table/DateCell.tsx
@@ -1,6 +1,11 @@
 import { CellContext } from "@tanstack/react-table";
 import * as fns from "date-fns";
 
-export default function DateCell<T>(ctx: CellContext<T, Date>) {
-  return fns.format(ctx.cell.getValue(), "dd-LL-yyyy");
+export default function DateCell<T>(ctx: CellContext<T, Date | String>) {
+  // If value is of type date, format it
+  if (ctx.cell.getValue() instanceof Date) {
+    return fns.format(ctx.cell.getValue() as Date, "yyyy-MM-dd");
+  } else {
+    return ctx.cell.getValue();
+  }
 }
