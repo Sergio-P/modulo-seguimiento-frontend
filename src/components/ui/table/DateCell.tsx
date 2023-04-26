1ff29165388f59c7c70ed1128a7e85c4086db737
ElficTitious <ismael.correa.25@gmail.com>
2023-04-25 21:38:19 -0400

    fix: fixed return type of DateCell function

--- src/components/ui/table/DateCell.tsx
+++ src/components/ui/table/DateCell.tsx
@@ -1,11 +1,11 @@
 import { CellContext } from "@tanstack/react-table";
 import * as fns from "date-fns";
 
-export default function DateCell<T>(ctx: CellContext<T, Date | String>) {
+export default function DateCell<T>(ctx: CellContext<T, Date | string>) {
   // If value is of type date, format it
   if (ctx.cell.getValue() instanceof Date) {
     return fns.format(ctx.cell.getValue() as Date, "yyyy-MM-dd");
   } else {
-    return ctx.cell.getValue();
+    return ctx.cell.getValue() as string;
   }
 }
