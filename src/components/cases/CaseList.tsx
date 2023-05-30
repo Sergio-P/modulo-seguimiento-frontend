2870ea346f3ec4eab18c5bc798d929b83c331b21
Nicolás Lemuñir <nicolas.lemunir2112@gmail.com>
2023-05-30 13:00:43 -0400

    feat(assignment): don't show assignment to non-admins

--- src/components/cases/CaseList.tsx
+++ src/components/cases/CaseList.tsx
@@ -8,7 +8,7 @@ import {
 import _ from "lodash";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useQuery } from "react-query";
 import Button from "../ui/Button";
 import Checkbox from "../ui/Checkbox";
@@ -22,6 +22,7 @@ import AssignmentModal from "./CaseList/AssignmentModal";
 import { SeguimientoState } from "@/types/Enums";
 import axiosClient from "@/utils/axios";
 import LogoutButton from "../auth/LogoutButton";
+import { useUser } from "@/hooks/auth";
 
 export default function CaseList() {
   const caseQuery = useQuery({
@@ -51,108 +52,116 @@ export default function CaseList() {
 }
 
 const columnHelper = createColumnHelper<Seguimiento>();
-const columns = [
-  columnHelper.display({
-    id: "checkbox",
-    size: 48,
-    header: (props) => (
-      <div className="flex justify-center">
-        <Checkbox
-          type="checkbox"
-          sizeType="sm"
-          onClick={props.table.getToggleAllRowsSelectedHandler()}
-          checked={props.table.getIsAllRowsSelected()}
-        />
-      </div>
-    ),
-    cell: (props) => (
-      <div className="flex justify-center">
-        <Checkbox
-          type="checkbox"
-          sizeType="sm"
-          onClick={props.row.getToggleSelectedHandler()}
-          checked={props.row.getIsSelected()}
-          disabled={!props.row.getCanSelect()}
-        />
-      </div>
-    ),
-  }),
-  columnHelper.display({
-    id: "boton_ver",
-    header: "Ver",
-    size: 32,
-    cell: (props) => (
-      <Link
-        href={`/cases/${props.row.original.id}`}
-        className="block h-6 w-6 text-primary"
-      >
-        <Image
-          alt=""
-          src="/icons/View.svg"
-          width={24}
-          height={24}
-          className="h-6 w-6"
-        />
-      </Link>
-    ),
-  }),
-  columnHelper.accessor("caso_registro_correspondiente.ficha", {
-    header: "Ficha",
-    size: 100,
-  }),
-  columnHelper.accessor("state", {
-    header: "Estado",
-    size: 100,
-  }),
-  columnHelper.accessor(
-    (row) =>
-      `${row.caso_registro_correspondiente.nombre} ${row.caso_registro_correspondiente.apellido}`,
-    {
-      id: "paciente",
-      header: "Paciente",
-      size: 128,
-    }
-  ),
-  columnHelper.accessor("caso_registro_correspondiente.subcategoria", {
-    header: "Subcategoría",
-    size: 168,
-  }),
-  columnHelper.accessor("caso_registro_correspondiente.fecha_dg", {
-    header: "Fecha diagnóstico",
-    size: 110,
-    cell: dateCell,
-  }),
-  columnHelper.accessor("usuario_asignado.nombre", {
-    header: "Usuario asignado",
-    size: 128,
-  }),
-  columnHelper.accessor("tiene_consulta_nueva", {
-    header: "Consulta Nueva",
-    size: 64,
-    cell: BooleanCell,
-  }),
-  columnHelper.accessor("tiene_examenes", {
-    header: "Examenes",
-    size: 64,
-    cell: BooleanCell,
-  }),
-  columnHelper.accessor("tiene_comite_oncologico", {
-    header: "Comité Onc.",
-    size: 64,
-    cell: BooleanCell,
-  }),
-  columnHelper.accessor("tiene_tratamiento", {
-    header: "Tratamiento",
-    size: 64,
-    cell: BooleanCell,
-  }),
-];
 
 interface CaseListTableProps {
   data: Seguimiento[];
 }
 
 function CaseListTable({ data }: CaseListTableProps) {
+  const userQuery = useUser();
+  const columns = useMemo(
+    () => [
+      ...(userQuery.data?.rol === "admin"
+        ? [
+            columnHelper.display({
+              id: "checkbox",
+              size: 48,
+              header: (props) => (
+                <div className="flex justify-center">
+                  <Checkbox
+                    type="checkbox"
+                    sizeType="sm"
+                    onClick={props.table.getToggleAllRowsSelectedHandler()}
+                    checked={props.table.getIsAllRowsSelected()}
+                  />
+                </div>
+              ),
+              cell: (props) => (
+                <div className="flex justify-center">
+                  <Checkbox
+                    type="checkbox"
+                    sizeType="sm"
+                    onClick={props.row.getToggleSelectedHandler()}
+                    checked={props.row.getIsSelected()}
+                    disabled={!props.row.getCanSelect()}
+                  />
+                </div>
+              ),
+            }),
+          ]
+        : []),
+      columnHelper.display({
+        id: "boton_ver",
+        header: "Ver",
+        size: 32,
+        cell: (props) => (
+          <Link
+            href={`/cases/${props.row.original.id}`}
+            className="block h-6 w-6 text-primary"
+          >
+            <Image
+              alt=""
+              src="/icons/View.svg"
+              width={24}
+              height={24}
+              className="h-6 w-6"
+            />
+          </Link>
+        ),
+      }),
+      columnHelper.accessor("caso_registro_correspondiente.ficha", {
+        header: "Ficha",
+        size: 100,
+      }),
+      columnHelper.accessor("state", {
+        header: "Estado",
+        size: 100,
+      }),
+      columnHelper.accessor(
+        (row) =>
+          `${row.caso_registro_correspondiente.nombre} ${row.caso_registro_correspondiente.apellido}`,
+        {
+          id: "paciente",
+          header: "Paciente",
+          size: 128,
+        }
+      ),
+      columnHelper.accessor("caso_registro_correspondiente.subcategoria", {
+        header: "Subcategoría",
+        size: 168,
+      }),
+      columnHelper.accessor("caso_registro_correspondiente.fecha_dg", {
+        header: "Fecha diagnóstico",
+        size: 110,
+        cell: dateCell,
+      }),
+      columnHelper.accessor("usuario_asignado.nombre", {
+        header: "Usuario asignado",
+        size: 128,
+      }),
+      columnHelper.accessor("tiene_consulta_nueva", {
+        header: "Consulta Nueva",
+        size: 64,
+        cell: BooleanCell,
+      }),
+      columnHelper.accessor("tiene_examenes", {
+        header: "Examenes",
+        size: 64,
+        cell: BooleanCell,
+      }),
+      columnHelper.accessor("tiene_comite_oncologico", {
+        header: "Comité Onc.",
+        size: 64,
+        cell: BooleanCell,
+      }),
+      columnHelper.accessor("tiene_tratamiento", {
+        header: "Tratamiento",
+        size: 64,
+        cell: BooleanCell,
+      }),
+    ],
+    [userQuery.data]
+  );
   const table = useReactTable({
     data,
     columns,
@@ -175,21 +184,23 @@ function CaseListTable({ data }: CaseListTableProps) {
         title="Lista de Casos"
         extraHeader={
           <div>
-            <Button
-              filled
-              disabled={!_.some(Object.values(table.getState().rowSelection))}
-              onClick={() => {
-                let selected = Object.keys(table.getState().rowSelection).map(
-                  (id: string) => {
-                    return table.getRowModel().rowsById[id].original.id;
-                  }
-                );
-                setSelectedIds(selected);
-                setModalOpen(true);
-              }}
-            >
-              Asignar seguimiento
-            </Button>
+            {userQuery.data?.rol === "admin" && (
+              <Button
+                filled
+                disabled={!_.some(Object.values(table.getState().rowSelection))}
+                onClick={() => {
+                  let selected = Object.keys(table.getState().rowSelection).map(
+                    (id: string) => {
+                      return table.getRowModel().rowsById[id].original.id;
+                    }
+                  );
+                  setSelectedIds(selected);
+                  setModalOpen(true);
+                }}
+              >
+                Asignar seguimiento
+              </Button>
+            )}
           </div>
         }
       />
