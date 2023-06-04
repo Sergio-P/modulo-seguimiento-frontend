29a09702b59edb74dd964ce4b8025e3d129f68b6
Nicolás Lemuñir <nicolas.lemunir2112@gmail.com>
2023-06-03 21:14:54 -0400

    refactor(Modal): create generic Modal component for creating modals

--- src/components/cases/CaseForm/modals/MoreInfoModal.tsx
+++ src/components/cases/CaseForm/modals/MoreInfoModal.tsx
@@ -1,85 +1,66 @@
-import Button from "@/components/ui/Button";
-import CustomDialog from "@/components/ui/CustomDialog";
+import Modal from "@/components/ui/Modal";
 import { Seguimiento } from "@/types/Seguimiento";
-import { useState } from "react";
 
 interface MoreInfoModalProps {
   seguimiento: Seguimiento;
 }
 
 export default function MoreInfoModal(props: MoreInfoModalProps) {
-  const [open, setOpen] = useState(false);
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const handleOpen = () => {
-    setOpen(true);
-  };
   const caso = props.seguimiento.caso_registro_correspondiente;
   return (
-    <>
-      <Button
-        className="w-48 place-self-center"
-        type="button"
-        filled
-        onClick={handleOpen}
-      >
-        Más Información
-      </Button>
-      <CustomDialog
-        open={open}
-        onClose={handleClose}
-        title="Antecedentes Personales"
-        width="lg"
-      >
-        <div className="grid w-full grid-cols-6 gap-4 rounded-2xl p-2 text-left">
-          <div className="contents">
-            <div className="font-bold">Nombre Paciente</div>
-            <div className="col-span-5">
-              {caso?.nombre} {caso?.apellido}
-            </div>
-          </div>
-          <Separator />
-          <div className="contents">
-            <div className="font-bold">Ficha</div>
-            <div className="">{caso?.ficha}</div>
-            <div className="font-bold">RUT/DNI</div>
-            <div className="">{caso?.rut_dni}</div>
-            <div className="font-bold">N° Registro</div>
-            <div className="">{caso?.num_registro}</div>
-          </div>
-          <Separator />
-          <div className="contents">
-            <div className="font-bold">Categoría</div>
-            <div className="col-span-5">{caso?.categoria}</div>
-          </div>
-          <Separator />
-          <div className="contents">
-            <div className="font-bold">Subcategoría</div>
-            <div className="col-span-5">{caso?.subcategoria}</div>
-          </div>
-          <Separator />
-          <div className="contents">
-            <div className="font-bold">Fecha Diagnóstico</div>
-            <div className="">{caso?.fecha_dg.toString()}</div>
-            <div className="font-bold">Lateralidad</div>
-            <div className="">{caso?.lateralidad}</div>
-            <div className="font-bold">Estadío Diagnóstico</div>
-            <div className="">{caso?.estadio_dg}</div>
-          </div>
-          <Separator />
-          <div className="contents">
-            <div className="font-bold">Morfología</div>
-            <div className="col-span-5">{caso?.morfologia}</div>
-          </div>
-          <Separator />
-          <div className="contents">
-            <div className="font-bold">Topografía</div>
-            <div className="col-span-5">{caso?.topografia}</div>
+    <Modal
+      className="w-48 place-self-center"
+      buttonContent="Más Información"
+      title="Antecedentes Personales"
+      width="lg"
+    >
+      <div className="grid w-full grid-cols-6 gap-4 rounded-2xl p-2 text-left">
+        <div className="contents">
+          <div className="font-bold">Nombre Paciente</div>
+          <div className="col-span-5">
+            {caso?.nombre} {caso?.apellido}
           </div>
         </div>
-      </CustomDialog>
-    </>
+        <Separator />
+        <div className="contents">
+          <div className="font-bold">Ficha</div>
+          <div className="">{caso?.ficha}</div>
+          <div className="font-bold">RUT/DNI</div>
+          <div className="">{caso?.rut_dni}</div>
+          <div className="font-bold">N° Registro</div>
+          <div className="">{caso?.num_registro}</div>
+        </div>
+        <Separator />
+        <div className="contents">
+          <div className="font-bold">Categoría</div>
+          <div className="col-span-5">{caso?.categoria}</div>
+        </div>
+        <Separator />
+        <div className="contents">
+          <div className="font-bold">Subcategoría</div>
+          <div className="col-span-5">{caso?.subcategoria}</div>
+        </div>
+        <Separator />
+        <div className="contents">
+          <div className="font-bold">Fecha Diagnóstico</div>
+          <div className="">{caso?.fecha_dg.toString()}</div>
+          <div className="font-bold">Lateralidad</div>
+          <div className="">{caso?.lateralidad}</div>
+          <div className="font-bold">Estadío Diagnóstico</div>
+          <div className="">{caso?.estadio_dg}</div>
+        </div>
+        <Separator />
+        <div className="contents">
+          <div className="font-bold">Morfología</div>
+          <div className="col-span-5">{caso?.morfologia}</div>
+        </div>
+        <Separator />
+        <div className="contents">
+          <div className="font-bold">Topografía</div>
+          <div className="col-span-5">{caso?.topografia}</div>
+        </div>
+      </div>
+    </Modal>
   );
 }
 
---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Button from "./Button";
+import _ from "lodash";
+import CustomDialog from "./CustomDialog";
+
+interface ModalProps {
+  buttonContent: string | React.ReactNode;
+  title: string;
+  width?: "md" | "lg";
+}
+
+export default function Modal(
+  props: ModalProps & React.ButtonHTMLAttributes<HTMLButtonElement>
+) {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  return (
+    <>
+      <Button
+        type="button"
+        filled
+        onClick={handleOpen}
+        {..._.omit(props, "children", "buttonContent", "title", "width")}
+      >
+        {props.buttonContent}
+      </Button>
+      <CustomDialog
+        open={open}
+        onClose={handleClose}
+        title={props.title}
+        width={props.width}
+      >
+        {props.children}
+      </CustomDialog>
+    </>
+  );
+}
