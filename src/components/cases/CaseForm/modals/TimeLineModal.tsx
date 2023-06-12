1f8820fc3e9cb6619e73f6dc167acdf0fa82f586
diego_cl <diegoclrey@hotmail.com>
2023-06-12 18:04:47 -0400

    feat: refactored timeline and added custom display for lists

--- src/components/cases/CaseForm/modals/TimeLineModal.tsx
+++ src/components/cases/CaseForm/modals/TimeLineModal.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import Image from "next/image";
 import Modal, { ModalProps } from "@/components/ui/Modal";
 import BoundingBox from "@/components/ui/layout/BoundingBox";
 import { Seguimiento } from "@/types/Seguimiento";
@@ -7,6 +8,12 @@ import { Subtitle, Foo } from "../ui";
 import MoreInfoModal from "./MoreInfoModal";
 import { SeguimientoContext } from "../context/seguimiento";
 import TimeLine from "../../TimeLine";
+import { Disclosure, Transition, Tab } from "@headlessui/react";
+import ComiteList from "../lists/ComiteList";
+import MetastasisList from "../lists/MetastasisList";
+import ProgresionList from "../lists/ProgresionList";
+import RecurrenciaList from "../lists/RecurrenciaList";
+import TratamientoEnFALPList from "../lists/TratamientoEnFALPList";
 
 interface TimeLineModalProps extends Partial<ModalProps> {
   seguimiento: Seguimiento;
@@ -23,24 +30,51 @@ export default function TimeLineModal(props: TimeLineModalProps) {
         render={(props) => (
         <div>
             <BoundingBox thin className="m-4 border-background-dark">
-                <div className="flex place-items-center justify-around">
-                    <div className="flex-col items-center justify-center">
-                        <h2 className="text-2xl font-bold">
-                            {caso?.nombre} {caso?.apellido}
-                        </h2>
-                        <Subtitle label={"Seguimiento"} value={seguimientoData?.numero_seguimiento?.toString() || ""} />
-                    </div>
-                    <Foo label={"RUT"} value={caso?.rut_dni || ""} />
-                    <Foo
-                        label={"Ficha"}
-                        value={caso?.ficha.toString() || ""} />
-                    <Foo
-                        label={"Subcategoría"}
-                        value={caso?.subcategoria || ""} />
-                    <Foo
-                        label={"Lateralidad"}
-                        value={caso?.lateralidad || ""} />
-                </div>
+                <Disclosure>
+                    {({ open }) => (
+                        <>
+                            <div className="flex place-items-center justify-around">
+                                <div className="flex-col items-center justify-center">
+                                    <h2 className="text-2xl font-bold">
+                                        {caso?.nombre} {caso?.apellido}
+                                    </h2>
+                                    <Subtitle label={"Seguimiento"} value={seguimientoData?.numero_seguimiento?.toString() || ""} />
+                                </div>
+                                <Foo label={"RUT"} value={caso?.rut_dni || ""} />
+                                <Foo
+                                    label={"Ficha"}
+                                    value={caso?.ficha.toString() || ""} />
+                                <Foo
+                                    label={"Subcategoría"}
+                                    value={caso?.subcategoria || ""} />
+                                <Foo
+                                    label={"Lateralidad"}
+                                    value={caso?.lateralidad || ""} />
+                                <Disclosure.Button className="flex w-6 h-6 rounded-lg border-bg-dark bg-primary">
+                                    <Image
+                                        src={`/icons/plus.svg`}
+                                        width={16}
+                                        height={16}
+                                        alt=""
+                                        className="m-auto h-4 w-4"
+                                    />
+                                </Disclosure.Button>
+                            </div>
+                            <Transition
+                                enter="transition duration-100 ease-out"
+                                enterFrom="transform scale-95 opacity-0"
+                                enterTo="transform scale-100 opacity-100"
+                                leave="transition duration-75 ease-out"
+                                leaveFrom="transform scale-100 opacity-100"
+                                leaveTo="transform scale-95 opacity-0"
+                            >
+                                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                                    <h1 className="text-2xl font-bold text-center">Info</h1>      
+                                </Disclosure.Panel>
+                            </Transition>
+                        </>     
+                    )}
+                </Disclosure>
             </BoundingBox>
             <TimeLine />
         </div>
