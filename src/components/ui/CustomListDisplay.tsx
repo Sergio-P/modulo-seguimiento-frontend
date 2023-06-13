59d672aca2d67a1ea3f0614ade1e94751dedd9e4
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-06-13 11:01:51 -0400

    feat(CustomListDisplay): Added first styles on Metastasis Tabs

--- src/components/ui/CustomListDisplay.tsx
+++ src/components/ui/CustomListDisplay.tsx
@@ -7,58 +7,83 @@ import TratamientoEnFALPList from "../cases/CaseForm/lists/TratamientoEnFALPList
 import { useContext } from "react";
 import { SeguimientoContext } from "../cases/CaseForm/context/seguimiento";
 import { UpdateDataContext } from "../cases/CaseForm/context/updateData";
-import BoundingBox from "./layout/BoundingBox";
-import { Section } from "../cases/CaseForm/ui";
 
 interface CustomListDisplayProps {
-    origen: number | null;
+  origen: number | null;
+}
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function CustomListDisplay(props: CustomListDisplayProps) {
-    const seguimiento = useContext(SeguimientoContext)
-    const updateData = useContext(UpdateDataContext)
-    console.log("origen", props.origen)
-    return (
-        <div className="m-4 border rounded-lg border-background-dark">
-            <Disclosure>
-                {({ open }) => (
-                    <>
-                        <Disclosure.Button className="flex w-full justify-between rounded-lg bg-background px-4 py-2 text-left text-md font-medium text-font-subtitle border-background-dark hover:bg-background-dark focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                            {props.origen === null || props.origen === undefined ? <span>Registro</span> : <span>Seguimiento {props.origen}</span>}
-                        </Disclosure.Button>
-                        <Transition
-                            enter="transition duration-100 ease-out"
-                            enterFrom="transform scale-95 opacity-0"
-                            enterTo="transform scale-100 opacity-100"
-                            leave="transition duration-75 ease-out"
-                            leaveFrom="transform scale-100 opacity-100"
-                            leaveTo="transform scale-95 opacity-0"
-                        >
-                            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                                <Tab.Group>
-                                    <Tab.List className={
-                                        'flex gap-4'
-                                        }>
-                                        <Tab>Metástasis</Tab>
-                                        <Tab>Recurrencia</Tab>
-                                        <Tab>Progresión</Tab>
-                                        <Tab>Comité</Tab>
-                                        <Tab>Tratamiento</Tab>
-                                    </Tab.List>
-                                    <Tab.Panels>
-                                        <Tab.Panel><MetastasisList/></Tab.Panel>
-                                        <Tab.Panel><RecurrenciaList/></Tab.Panel>
-                                        <Tab.Panel><ProgresionList/></Tab.Panel>
-                                        <Tab.Panel><ComiteList/></Tab.Panel>
-                                        <Tab.Panel><TratamientoEnFALPList/></Tab.Panel>
-                                    </Tab.Panels>
-                                </Tab.Group>        
-                            </Disclosure.Panel>
-                        </Transition>
-                    </>     
-                )}
-            </Disclosure>
-        </div>
-    );
+  const seguimiento = useContext(SeguimientoContext);
+  const updateData = useContext(UpdateDataContext);
+  console.log("origen", props.origen);
+  return (
+    <div className="m-4 rounded-lg border border-background-dark">
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="text-md flex w-full justify-between rounded-lg border-background-dark bg-background px-4 py-2 text-left font-medium text-font-subtitle hover:bg-background-dark focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+              {props.origen === null || props.origen === undefined ? (
+                <span>Registro</span>
+              ) : (
+                <span>Seguimiento {props.origen}</span>
+              )}
+            </Disclosure.Button>
+            <Transition
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+            >
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                <Tab.Group>
+                  <Tab.List className={"flex gap-4"}>
+                    <Tab
+                      className={({ selected }) =>
+                        classNames(
+                          "text-sm text-gray-500",
+                          "focus:outline-none focus:ring-2",
+                          selected
+                            ? "underline decoration-accent text-accent bg-white shadow"
+                            : "hover:underline"
+                        )
+                      }
+                    >
+                      Metástasis Pulento
+                    </Tab>
+                    <Tab>Recurrencia</Tab>
+                    <Tab>Progresión</Tab>
+                    <Tab>Comité</Tab>
+                    <Tab>Tratamiento</Tab>
+                  </Tab.List>
+                  <Tab.Panels>
+                    <Tab.Panel>
+                      <MetastasisList />
+                    </Tab.Panel>
+                    <Tab.Panel>
+                      <RecurrenciaList />
+                    </Tab.Panel>
+                    <Tab.Panel>
+                      <ProgresionList />
+                    </Tab.Panel>
+                    <Tab.Panel>
+                      <ComiteList />
+                    </Tab.Panel>
+                    <Tab.Panel>
+                      <TratamientoEnFALPList />
+                    </Tab.Panel>
+                  </Tab.Panels>
+                </Tab.Group>
+              </Disclosure.Panel>
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+    </div>
+  );
 }
-
