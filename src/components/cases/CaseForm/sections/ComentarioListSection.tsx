a1f9e5e721cae55ecfa0f0cff56f4d00b3f4c7a2
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-07-03 11:22:45 -0400

    style: increased gap on comentarios

--- src/components/cases/CaseForm/sections/ComentarioListSection.tsx
+++ src/components/cases/CaseForm/sections/ComentarioListSection.tsx
@@ -12,7 +12,7 @@ export default function ComentarioListSection({}: ComentarioListProps) {
   console.log("Comentarios: ", seguimiento?.caso_registro_correspondiente?.comentarios);
   const comentarios = seguimiento?.caso_registro_correspondiente?.comentarios;
   if (!comentarios) {
-    return <div>No hay comentarios aún.</div>;
+    return <div className="font-semibold text-font-subtitle">No hay comentarios aún.</div>;
   }
   return (
       <div className="h-[60vh] overflow-y-scroll">
@@ -21,7 +21,7 @@ export default function ComentarioListSection({}: ComentarioListProps) {
             {comentarios.map((comentario) => (
               <div className="flex-col rounded-lg bg-background-dark px-3 py-2 m-4">
                 <div className="text-lg">{comentario.comentario}</div>
-                <div className="flex pt-3 justify-end gap-2 font-bold text-font-subtitle">
+                <div className="flex pt-3 justify-end gap-3 font-semibold text-font-subtitle">
                   <div>{comentario.nombre_usuario}</div>
                   <div>{fns.format(new Date(comentario.created_at), "dd-MM-yyyy HH:mm")}</div>
                   <div>{comentario.numero_seguimiento > 0 ? `Seguimiento ${comentario.numero_seguimiento}` : "Registro"}</div>
